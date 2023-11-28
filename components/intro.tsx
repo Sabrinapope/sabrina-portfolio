7be5800f40403b16bbc3f27bca01457f93974968
Sabrina Pope <sabrinapope@Sabrinas-MacBook-Air.local>
2023-11-28 15:07:56 -0500

    Updating website

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,17 +4,18 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Sabrina Pope Portfolio.
+        Sabrina Pope's Tech Portfolio.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Checkout my LinkedIn{' '}
+        Checkout my LinkedIn profile. {' '}
         <a
-          href="https://www.linkedin.com/in/sabrina-pope/"
+          href="https://www.linkedin.com/in/sabrina-pope/" 
           className="underline hover:text-blue-600 duration-200 transition-colors"
+          target="_blank"
         >
-      
+                 
         </a>{' '}
-        and {CMS_NAME}.
+            {CMS_NAME}.   
       </h4>
     </section>
   )
