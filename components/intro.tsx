339030ff222de23fcd32291d3be132cd2c4ff598
Sabrina Pope <sabrinapope@sabrinas-air.attlocal.net>
2023-11-27 21:02:04 -0500

    updating landing page

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,15 +4,15 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Sabrina Pope Portfolio.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        Checkout my LinkedIn{' '}
         <a
-          href="https://nextjs.org/"
+          href="https://www.linkedin.com/in/sabrina-pope/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
+      
         </a>{' '}
         and {CMS_NAME}.
       </h4>
